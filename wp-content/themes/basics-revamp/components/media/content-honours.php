da4d1c7a0380110690f564fa7c44130a30d83d19
Mayank Panchal <mayank@screetract.com>
2024-10-14 01:54:04 +0530

    Cahnges update

--- wp-content/themes/basics-revamp/components/media/content-honours.php
+++ wp-content/themes/basics-revamp/components/media/content-honours.php
@@ -14,16 +14,17 @@ $custom_posts = get_posts($args);
         // echo $post->post_title;
         // echo $post->post_content;
 
+        $post_id = get_the_ID();
     ?>
 
-    <div class="media-wrap">
-        <img src="<?php echo get_the_post_thumbnail_url($post->ID, 'large') ?>" alt="<?php echo $post->post_title ?>">
-        <div class="media-wrap-content">
-            <h3><?php echo $post->post_title ?></h3>
-            <h5><?php echo wp_trim_words($post->post_content, 20) ?>...</h5>
-            <a class="btn btn-white" target="_blank" href="<?php the_permalink($post->ID) ?>">Read More</a>
+        <div class="media-wrap">
+            <img src="<?php echo get_the_post_thumbnail_url($post->ID, 'large') ?>" alt="<?php echo $post->post_title ?>">
+            <div class="media-wrap-content">
+                <h3><?php echo $post->post_title ?></h3>
+                <h5><?php echo wp_trim_words($post->post_content, 20) ?>...</h5>
+                <a class="btn btn-white" target="_blank" href="<?php the_field('source_uri', $post_id); ?>">Read More</a>
+            </div>
         </div>
-    </div>
     <?php
         // ...
     }
