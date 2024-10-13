0248ee8a6ae99b2aea957fc033c5119dc9395700
Mayank Panchal <mayank@screetract.com>
2024-10-14 01:53:23 +0530

    Changes update

--- wp-content/themes/basics-revamp/components/media/content-publication.php
+++ wp-content/themes/basics-revamp/components/media/content-publication.php
@@ -2,6 +2,7 @@
 $args = array(
     'post_type' => 'publication',
     'posts_per_page' => -1, // Fetch all posts
+    'order' => 'asc'
 );
 $custom_posts = get_posts($args);
 
@@ -14,16 +15,22 @@ $custom_posts = get_posts($args);
         // echo $post->post_title;
         // echo $post->post_content;
 
+        $post_id = get_the_ID();
+
+
     ?>
 
-    <div class="media-wrap">
-        <img src="<?php echo get_the_post_thumbnail_url($post->ID, 'large') ?>" alt="<?php echo $post->post_title ?>">
-        <div class="media-wrap-content">
-            <h3><?php echo $post->post_title ?></h3>
-            <h5><?php echo wp_trim_words($post->post_content, 20) ?>...</h5>
-            <a class="btn btn-white" target="_blank" href="<?php the_permalink($post->ID) ?>">Read More</a>
+        <div class="media-wrap">
+            <img src="<?php echo get_the_post_thumbnail_url($post->ID, 'large') ?>" alt="<?php echo $post->post_title ?>">
+            <div class="media-wrap-content">
+                <h3><?php echo $post->post_title ?></h3>
+                <h5><?php echo wp_trim_words($post->post_content, 20) ?>...</h5>
+                <a class="btn btn-white" target="_blank" href="<?php the_field('source_uri', $post_id);; ?>">
+                    Read
+                    More
+                </a>
+            </div>
         </div>
-    </div>
     <?php
         // ...
     }
