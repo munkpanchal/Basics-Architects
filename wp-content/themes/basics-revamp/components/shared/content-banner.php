717632e97b542e63939cbda99ceae2d0da662bd7
Mayank Panchal <mayank@screetract.com>
2024-10-14 00:10:03 +0530

    Changes Update

--- wp-content/themes/basics-revamp/components/shared/content-banner.php
+++ wp-content/themes/basics-revamp/components/shared/content-banner.php
@@ -3,6 +3,7 @@ $bannerUri = get_theme_file_uri("/public/banner.jpg");
 ?>
 <section class="banner">
     <div class="banner-wrapper" style="--bannerUri:url(<?php echo $bannerUri ?>)">
+        <img src="<?php echo $bannerUri ?>" alt="">
 
     </div>
 </section>
