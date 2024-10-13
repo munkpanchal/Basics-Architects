37929f4ab580c3d9b7851f9f04b43980c4c2e649
Mayank Panchal <mayank@screetract.com>
2024-10-13 23:42:40 +0530

    post page changes update

--- wp-content/themes/basics-revamp/single-post.php
+++ wp-content/themes/basics-revamp/single-post.php
@@ -4,6 +4,9 @@
         <h2>
             <?php the_title() ?>
         </h2>
+        <!-- <p><strong>Author:</strong> <?php echo get_the_author(); ?></p> -->
+        <p><strong>Author:</strong> Basics Architects</p>
+        <p><strong>Published Date:</strong> <?php echo get_the_date('F j, Y'); ?></p>
     </div>
     <div class="single-blog-page-content">
         <?php
