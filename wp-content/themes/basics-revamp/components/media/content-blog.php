a26fd92b2782bdfdb86c286bf03c3531675e52ce
Mayank Panchal <mayank@screetract.com>
2024-10-14 00:05:25 +0530

    Latest changes update

--- wp-content/themes/basics-revamp/components/media/content-blog.php
+++ wp-content/themes/basics-revamp/components/media/content-blog.php
@@ -14,16 +14,22 @@ $custom_posts = get_posts($args);
         // echo $post->post_title;
         // echo $post->post_content;
 
+        if (get_the_post_thumbnail_url($post->ID, 'large')) {
+            $imgUrl = get_the_post_thumbnail_url($post->ID, 'large');
+        } else {
+            $imgUrl = get_theme_file_uri("/public/default-blog.jpg");
+        }
+
     ?>
 
-    <div class="media-wrap">
-        <img src="<?php echo get_the_post_thumbnail_url($post->ID, 'large') ?>" alt="<?php echo $post->post_title ?>">
-        <div class="media-wrap-content">
-            <h3><?php echo $post->post_title ?></h3>
-            <h5><?php echo wp_trim_words($post->post_content, 20) ?>...</h5>
-            <a class="btn btn-white" target="_blank" href="<?php the_permalink($post->ID) ?>">Read More</a>
+        <div class="media-wrap">
+            <img src="<?php echo $imgUrl ?>" alt="<?php echo $post->post_title ?>">
+            <div class="media-wrap-content">
+                <h3><?php echo $post->post_title ?></h3>
+                <h5><?php echo wp_trim_words($post->post_content, 20) ?>...</h5>
+                <a class="btn btn-white" target="_blank" href="<?php the_permalink($post->ID) ?>">Read More</a>
+            </div>
         </div>
-    </div>
     <?php
         // ...
     }
