1d69f7032d6dd920e041eab7ba860e278ce103ed
Mayank Panchal <mayank@screetract.com>
2024-10-29 12:24:34 +0530

    Changes update

--- wp-content/themes/basics-revamp/templates/contact.php
+++ wp-content/themes/basics-revamp/templates/contact.php
@@ -56,6 +56,13 @@
                 </div>
             </div>
         </div>
+        <div class="contact-map">
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3505.138446425003!2d77.20447927414592!3d28.53555808848027!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce21cbc9c5eed%3A0x1409ce170c78a4dd!2sBasics%20Architects!5e0!3m2!1sen!2sin!4v1730184397819!5m2!1sen!2sin"
+                width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+
     </div>
 </main>
 <?php get_footer() ?>
