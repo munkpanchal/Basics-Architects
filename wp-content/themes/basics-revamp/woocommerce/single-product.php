1a98301c030af577ada4cf1dc14498965324a050
Mayank Panchal <mayank@screetract.com>
2024-11-14 16:07:26 +0530

    Changes iupd

--- wp-content/themes/basics-revamp/woocommerce/single-product.php
+++ wp-content/themes/basics-revamp/woocommerce/single-product.php
@@ -17,6 +17,11 @@ $attachment_ids = $product_id ? wc_get_product($product_id)->get_gallery_image_i
 
 <?php
 $bannerUri = get_theme_file_uri("/public/banner.webp");
+$thumbnail_url = get_the_post_thumbnail_url($product_id, 'full');
+if ($thumbnail_url) {
+    $bannerUri = $thumbnail_url;
+}
+
 ?>
 <section class="banner">
     <div class="container">
@@ -55,7 +60,7 @@ $bannerUri = get_theme_file_uri("/public/banner.webp");
                     <?php
                     if ($year) {
                     ?>
-                    <p data-aos="fade-up"><strong>Year : </strong><?php echo $year; ?></p>
+                        <p data-aos="fade-up"><strong>Year : </strong><?php echo $year; ?></p>
                     <?php
                     }
                     ?>
@@ -63,7 +68,7 @@ $bannerUri = get_theme_file_uri("/public/banner.webp");
                     <?php
                     if ($location) {
                     ?>
-                    <p data-aos="fade-up"><strong>Location : </strong><?php echo $location; ?></p>
+                        <p data-aos="fade-up"><strong>Location : </strong><?php echo $location; ?></p>
                     <?php
                     }
                     ?>
@@ -71,7 +76,7 @@ $bannerUri = get_theme_file_uri("/public/banner.webp");
                     <?php
                     if ($area) {
                     ?>
-                    <p data-aos="fade-up"><strong>Area : </strong><?php echo $area; ?></p>
+                        <p data-aos="fade-up"><strong>Area : </strong><?php echo $area; ?></p>
                     <?php
                     }
                     ?>
@@ -83,30 +88,30 @@ $bannerUri = get_theme_file_uri("/public/banner.webp");
             <?php
             if ($product->description) {
             ?>
-            <div class="sph-right">
+                <div class="sph-right">
 
-                <h2 class="project-title opacity-0 pointer-events-none max-sm:hidden">
-                    <?php
+                    <h2 class="project-title opacity-0 pointer-events-none max-sm:hidden">
+                        <?php
                         echo $product->get_name()
                         ?>
-                </h2>
-                <div class="project-content max-sm:pt-2">
+                    </h2>
+                    <div class="project-content max-sm:pt-2">
 
-                    <div data-aos="fade-up">
-                        <details>
-                            <summary>
-                                <?php
+                        <div data-aos="fade-up">
+                            <details>
+                                <summary>
+                                    <?php
                                     echo $product->short_description;
                                     ?>
-                            </summary>
-                            <?php
+                                </summary>
+                                <?php
                                 echo $product->description;
                                 ?>
-                        </details>
+                            </details>
 
+                        </div>
                     </div>
                 </div>
-            </div>
 
             <?php
             }
@@ -132,11 +137,11 @@ $bannerUri = get_theme_file_uri("/public/banner.webp");
                     $image_url = wp_get_attachment_url($attachment_id)
                 ?>
 
-                <a href="<?php echo $image_url; ?>" data-fancybox="gallery" data-caption="title"
-                    class="project <?php echo "project-" . floor(($i - 1) % 3) + 1 ?>" data-aos="fade-up">
-                    <img src=" <?php echo $image_url; ?>" alt="">
+                    <a href="<?php echo $image_url; ?>" data-fancybox="gallery" data-caption="title"
+                        class="project <?php echo "project-" . floor(($i - 1) % 3) + 1 ?>" data-aos="fade-up">
+                        <img src=" <?php echo $image_url; ?>" alt="">
 
-                </a>
+                    </a>
                 <?php
                     if ($i == 4) {
                         $i++;
@@ -186,19 +191,19 @@ $bannerUri = get_theme_file_uri("/public/banner.webp");
                     }
                 ?>
 
-                <div class="rp-box fade-box" data-aos="fade-up">
-                    <img src="<?php echo $imgUrl ?>" alt="<?php echo $product->get_name() ?>">
-                    <div class="rp-box-content fade-target">
-                        <h3 class="uppercase">
-                            <?php echo $product->get_name() ?>
-                        </h3>
-                        <p>
-                            <?php echo wp_trim_words($product->get_short_description(), 10) ?>
-                        </p>
-                        <a class="btn btn-white" target="_blank"
-                            href="<?php echo get_permalink($product->get_id()) ?>">Read More</a>
+                    <div class="rp-box fade-box" data-aos="fade-up">
+                        <img src="<?php echo $imgUrl ?>" alt="<?php echo $product->get_name() ?>">
+                        <div class="rp-box-content fade-target">
+                            <h3 class="uppercase">
+                                <?php echo $product->get_name() ?>
+                            </h3>
+                            <p>
+                                <?php echo wp_trim_words($product->get_short_description(), 10) ?>
+                            </p>
+                            <a class="btn btn-white" target="_blank"
+                                href="<?php echo get_permalink($product->get_id()) ?>">Read More</a>
+                        </div>
                     </div>
-                </div>
 
                 <?php
                 }
