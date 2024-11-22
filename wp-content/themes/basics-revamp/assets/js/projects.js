6366b4a7a2b993c6f03697694c2deae75d77b52d
Mayank Panchal <mayank@screetract.com>
2024-11-22 16:15:22 +0530

    Changes update

--- wp-content/themes/basics-revamp/assets/js/projects.js
+++ wp-content/themes/basics-revamp/assets/js/projects.js
@@ -3,6 +3,11 @@ import AOS from "aos";
 const projects = document.querySelectorAll(".project");
 
 const tabNavs = document.querySelectorAll("[data-tab-nav-project]");
+const tabBanners = document.querySelectorAll("[data-img]");
+const tabBannerNone = document.querySelector("[data-img-none]");
+const tabBannersContent = document.querySelectorAll("[data-content]");
+const tabBannerNoneContent = document.querySelector("[data-content-none]");
+
 let newTimeout;
 
 // Show all projects by default on page load
@@ -14,6 +19,50 @@ projects.forEach((project) => {
 tabNavs?.forEach((tabNav) => {
     tabNav.addEventListener("click", (e) => {
         const currentActive = tabNav.getAttribute("data-tab-target");
+
+        tabBanners?.forEach((tabBanner) => {
+            let innerActive = tabBanner.getAttribute("data-image-id");
+            if (innerActive == currentActive) {
+                if (!tabBanner.classList.contains("active")) {
+                    tabBanner.classList.add("active");
+                    AOS.refresh(); // Refresh AOS after changing image category
+                } else {
+                    tabBanners?.forEach((tabBanner) => {
+                        if (tabBanner.classList.contains("active")) {
+                            tabBanner.classList.remove("active");
+                            AOS.refresh(); // Refresh AOS after changing image category
+                        }
+                    });
+                    tabBannerNone.classList.add("active");
+                }
+            }
+        });
+        tabBannersContent?.forEach((tbc) => {
+            let innerActive = tbc.getAttribute("data-content-id");
+            console.log(innerActive);
+            if (innerActive == currentActive) {
+                if (!tbc.classList.contains("active")) {
+                    tabBannersContent?.forEach((tbci) => {
+                        let innerActive = tbci.getAttribute("data-content-id");
+                        if (innerActive !== currentActive) {
+                            if (tbci.classList.contains("active")) {
+                                tbci.classList.remove("active");
+                            }
+                        }
+                    });
+                    tbc.classList.add("active");
+                    AOS.refresh(); // Refresh AOS after changing image category
+                } else {
+                    tabBannersContent?.forEach((tbci) => {
+                        if (tbci.classList.contains("active")) {
+                            tbci.classList.remove("active");
+                            AOS.refresh(); // Refresh AOS after changing image category
+                        }
+                    });
+                    tabBannerNoneContent.classList.add("active");
+                }
+            }
+        });
 
         // Toggle the active class for the clicked tab
         if (tabNav.classList.contains("active")) {
---
+++ wp-content/themes/basics-revamp/woocommerce/taxonomy-product-cat.php
@@ -0,0 +1,166 @@
+<?php
+
+$current_category = get_queried_object();
+$category_id = $current_category->term_id;
+$args = [
+    'status' => 'publish',
+    'posts_per_page' => -1,
+    'orderby' => 'menu_order title',
+    'order'  => 'ASC',
+    'product_category_id' => $category_id,
+];
+
+$products = wc_get_products($args);
+
+
+
+$child_categories = get_categories(array(
+    'taxonomy'     => 'product_cat',
+    'parent'       => $category_id,
+    'hide_empty'    => false,
+));
+
+
+?>
+
+<?php get_header() ?>
+
+
+
+<div class="sub-menu-wrap ">
+    <div class="category-menu">
+
+        <?php
+        foreach ($child_categories as $category) {
+
+        ?>
+            <span class="menu-link" data-tab-nav-project data-tab-target="<?php echo $category->slug; ?>">
+                <?php
+                echo $category->name;
+                ?>
+            </span>
+        <?php
+        }
+        ?>
+    </div>
+</div>
+
+
+
+
+
+<?php
+$bannerUri = get_theme_file_uri("/public/banner.webp");
+?>
+<section class="banner">
+    <div class="container">
+
+        <div class="banner-wrapper sub-cat-banner" style="--bannerUri:url(<?php echo $bannerUri ?>)">
+            <?php
+            $thumbnail_id = get_term_meta($category_id, 'thumbnail_id', true);
+            $image_url = wp_get_attachment_url($thumbnail_id);
+
+
+            ?>
+
+            <div class="banner-content subcat active" data-content data-content-none data-content-id="none">
+                <h2 class="banner-heading !text-white" data-aos="fade-up">
+                    <?php
+                    echo $current_category->name;
+                    ?>
+                </h2>
+
+            </div>
+
+            <img src="<?php echo $image_url ?>" class="active" data-img data-img-none data-image-id="none" alt="">
+            <?php
+            foreach ($child_categories as $category) {
+                // var_dump($category);
+                $cat_id = $category->term_id;
+                $thumbnail_id = get_term_meta($cat_id, 'thumbnail_id', true);
+                $image_url = wp_get_attachment_url($thumbnail_id);
+            ?>
+                <img src="<?php echo $image_url ?>" data-img data-image-id="<?php echo $category->slug; ?>" alt="">
+
+                <div class="banner-content subcat" data-content data-content-id="<?php echo $category->slug; ?>">
+                    <h2 class="banner-heading !text-white" data-aos="fade-up">
+                        <?php
+                        echo $category->name;
+                        ?>
+                    </h2>
+
+
+                </div>
+
+            <?php
+            }
+
+
+            ?>
+
+
+
+        </div>
+    </div>
+</section>
+<?php
+
+?>
+
+<main class="main projects" id="projects">
+
+    <div class="container">
+
+        <?php
+
+        $totalProducts = wp_count_posts('product');
+        $totalProducts = $totalProducts->publish;
+        $totalProducts = 5 * $totalProducts;
+
+
+        ?>
+        <section class="projects-wrapper max-sm:mt-4">
+            <?php
+            foreach ($products as $product) {
+
+                $product_id = $product->get_id();
+                $product_categories = get_the_terms($product_id, 'product_cat');
+
+                $catName = $product_categories[0]->slug;
+
+
+                $thumbnail_url = get_the_post_thumbnail_url($product_id, 'full');
+
+
+
+            ?>
+                <div data-aos="fade-up" data-cat="<?php echo $catName; ?>" class="project active fade-box">
+                    <img src=" <?php echo $thumbnail_url ?>" alt="">
+                    <div class="project-content fade-target">
+                        <h3>
+                            <?php echo $product->name; ?>
+                        </h3>
+
+                        <p class="para ">
+                            <?php echo wp_trim_words($product->description, 20) ?>..
+                        </p>
+
+                        <a class="btn btn-white" target="_blank" href="<?php the_permalink($product_id) ?>">
+                            Read More
+                        </a>
+
+                    </div>
+                </div>
+            <?php
+
+            }
+            ?>
+        </section>
+
+
+
+    </div>
+
+
+</main>
+<?php get_footer() ?>
