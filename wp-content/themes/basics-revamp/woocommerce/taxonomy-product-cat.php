e5cf929647c88bb1ef16f6716af696969905f6e6
Mayank Panchal <mayank@screetract.com>
2024-11-22 21:57:09 +0530

    Change supdate

--- wp-content/themes/basics-revamp/woocommerce/taxonomy-product-cat.php
+++ wp-content/themes/basics-revamp/woocommerce/taxonomy-product-cat.php
@@ -34,11 +34,11 @@ $child_categories = get_categories(array(
         foreach ($child_categories as $category) {
 
         ?>
-            <span class="menu-link" data-tab-nav-project data-tab-target="<?php echo $category->slug; ?>">
-                <?php
+        <span class="menu-link" data-tab-nav-project data-tab-target="<?php echo $category->slug; ?>">
+            <?php
                 echo $category->name;
                 ?>
-            </span>
+        </span>
         <?php
         }
         ?>
@@ -63,11 +63,18 @@ $bannerUri = get_theme_file_uri("/public/banner.webp");
             ?>
 
             <div class="banner-content subcat active" data-content data-content-none data-content-id="none">
-                <h2 class="banner-heading !text-white" data-aos="fade-up">
+                <h2 class="banner-heading " data-aos="fade-up">
                     <?php
-                    echo $current_category->name;
+                    // echo $current_category->name;
                     ?>
                 </h2>
+                <p class="text-center banner-para">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Repudiandae illo maiores
+                    omnis dicta vel
+                    facilis assumenda alias reiciendis, numquam perspiciatis blanditiis optio quod est esse dolor et! A
+                    aliquam labore
+                    assumenda ducimus obcaecati.
+                </p>
 
             </div>
 
@@ -79,23 +86,23 @@ $bannerUri = get_theme_file_uri("/public/banner.webp");
                 $thumbnail_id = get_term_meta($cat_id, 'thumbnail_id', true);
                 $image_url = wp_get_attachment_url($thumbnail_id);
             ?>
-                <img src="<?php echo $image_url ?>" data-img data-image-id="<?php echo $category->slug; ?>" alt="">
+            <img src="<?php echo $image_url ?>" data-img data-image-id="<?php echo $category->slug; ?>" alt="">
 
-                <div class="banner-content subcat" data-content data-content-id="<?php echo $category->slug; ?>">
-                    <h2 class="banner-heading !text-white" data-aos="fade-up">
-                        <?php
+            <div class="banner-content subcat" data-content data-content-id="<?php echo $category->slug; ?>">
+                <h2 class="banner-heading !text-white" data-aos="fade-up">
+                    <?php
                         echo $category->name;
                         ?>
-                    </h2>
+                </h2>
 
-                    <p class="banner-para" data-aos="fade-up" data-aos-delay="100">
-                        <?php
+                <p class="banner-para" data-aos="fade-up" data-aos-delay="100">
+                    <?php
                         echo $category->description
                         ?>
-                    </p>
+                </p>
 
 
-                </div>
+            </div>
 
             <?php
             }
@@ -139,23 +146,23 @@ $bannerUri = get_theme_file_uri("/public/banner.webp");
 
 
             ?>
-                <div data-cat="<?php echo $catName; ?>" class="project active ">
-                    <img src=" <?php echo $thumbnail_url ?>" alt="">
-                    <div class="project-content fade-target">
-                        <h3>
-                            <?php echo $product->name; ?>
-                        </h3>
+            <div data-cat="<?php echo $catName; ?>" class="project active ">
+                <img src=" <?php echo $thumbnail_url ?>" alt="">
+                <div class="project-content fade-target">
+                    <h3>
+                        <?php echo $product->name; ?>
+                    </h3>
 
-                        <p class="para ">
-                            <?php echo wp_trim_words($product->description, 20) ?>..
-                        </p>
+                    <p class="para ">
+                        <?php echo wp_trim_words($product->description, 20) ?>..
+                    </p>
 
-                        <a class="btn btn-white" target="_blank" href="<?php the_permalink($product_id) ?>">
-                            Read More
-                        </a>
+                    <a class="btn btn-white" target="_blank" href="<?php the_permalink($product_id) ?>">
+                        Read More
+                    </a>
 
-                    </div>
                 </div>
+            </div>
             <?php
 
             }
