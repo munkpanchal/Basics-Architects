f9e483fa572378946f06fc8f1f4241d66846d4cc
Mayank Panchal <mayank@screetract.com>
2024-11-24 19:43:52 +0530

    Header file update

--- wp-content/themes/basics-revamp/header.php
+++ wp-content/themes/basics-revamp/header.php
@@ -6,7 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <?php wp_head(); ?>
-    <title><?php echo bloginfo('name'); ?></title>
+
+    <?php if (is_single()) : ?>
+        <title><?php the_title(); ?> | <?php bloginfo('description'); ?> | <?php bloginfo('name'); ?></title>
+    <?php elseif (is_page()) : ?>
+        <title><?php bloginfo('description'); ?> | <?php the_title(); ?> | <?php bloginfo('name'); ?></title>
+    <?php else : ?>
+        <title><?php bloginfo('name'); ?> | <?php bloginfo('description'); ?></title>
+    <?php endif; ?>
     <meta name="description" content="<?php echo bloginfo('description'); ?>">
     <link rel="shortcut icon" href="<?php echo get_theme_file_uri("/public/logo-ba.png") ?>" type="favicon">
 
