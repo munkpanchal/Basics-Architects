47d28923e7c6e43670b80c875e3805047b554985
Mayank Panchal <mayank@screetract.com>
2024-11-30 19:11:53 +0530

    Category update

--- wp-content/themes/basics-revamp/woocommerce/taxonomy-product-cat.php
+++ wp-content/themes/basics-revamp/woocommerce/taxonomy-product-cat.php
@@ -34,11 +34,11 @@ $child_categories = get_categories(array(
         foreach ($child_categories as $category) {
 
         ?>
-        <span class="menu-link" data-tab-nav-project data-tab-target="<?php echo $category->slug; ?>">
-            <?php
+            <span class="menu-link" data-tab-nav-project data-tab-target="<?php echo $category->slug; ?>">
+                <?php
                 echo $category->name;
                 ?>
-        </span>
+            </span>
         <?php
         }
         ?>
@@ -84,25 +84,25 @@ $bannerUri = get_theme_file_uri("/public/banner.webp");
                 $cat_id = $category->term_id;
                 $thumbnail_id = get_term_meta($cat_id, 'thumbnail_id', true);
                 $image_url = wp_get_attachment_url($thumbnail_id);
-                $catlabel =    get_field("gallery", $cat_id);
+                $catlabel =    get_field("catlabel", $cat_id);
             ?>
-            <img src="<?php echo $image_url ?>" data-img data-image-id="<?php echo $category->slug; ?>" alt="">
+                <img src="<?php echo $image_url ?>" data-img data-image-id="<?php echo $category->slug; ?>" alt="">
 
-            <div class="banner-content subcat" data-content data-content-id="<?php echo $category->slug; ?>">
-                <h2 class="banner-heading !text-white" data-aos="fade-up">
-                    <?php
+                <div class="banner-content subcat" data-content data-content-id="<?php echo $category->slug; ?>">
+                    <h2 class="banner-heading !text-white" data-aos="fade-up">
+                        <?php
                         echo $catlabel;
                         ?>
-                </h2>
+                    </h2>
 
-                <p class="banner-para" data-aos="fade-up" data-aos-delay="100">
-                    <?php
+                    <p class="banner-para" data-aos="fade-up" data-aos-delay="100">
+                        <?php
                         echo $category->description
                         ?>
-                </p>
+                    </p>
 
 
-            </div>
+                </div>
 
             <?php
             }
@@ -147,21 +147,21 @@ $bannerUri = get_theme_file_uri("/public/banner.webp");
             ?>
 
 
-            <div data-cat="<?php echo $catName; ?>" class="project active  fade-box">
-                <img src=" <?php echo $thumbnail_url ?>" alt="">
-                <div class="project-content fade-target">
-                    <h3>
-                        <?php echo $product->name; ?>
-                    </h3>
-                    <p class="para">
-                        <?php echo wp_trim_words($product->description, 20) ?>..
-                    </p>
-                    <a class="btn btn-white" target="_blank" href="<?php the_permalink($product_id) ?>">
-                        Read More
-                    </a>
+                <div data-cat="<?php echo $catName; ?>" class="project active  fade-box">
+                    <img src=" <?php echo $thumbnail_url ?>" alt="">
+                    <div class="project-content fade-target">
+                        <h3>
+                            <?php echo $product->name; ?>
+                        </h3>
+                        <p class="para">
+                            <?php echo wp_trim_words($product->description, 20) ?>..
+                        </p>
+                        <a class="btn btn-white" target="_blank" href="<?php the_permalink($product_id) ?>">
+                            Read More
+                        </a>
 
+                    </div>
                 </div>
-            </div>
             <?php
 
             }
