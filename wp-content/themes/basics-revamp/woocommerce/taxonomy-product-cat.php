141bf0bed51ad5190030fb8f951bb70d18dc1647
Mayank Panchal <mayank@screetract.com>
2024-11-30 19:17:43 +0530

    abc

--- wp-content/themes/basics-revamp/woocommerce/taxonomy-product-cat.php
+++ wp-content/themes/basics-revamp/woocommerce/taxonomy-product-cat.php
@@ -85,6 +85,7 @@ $bannerUri = get_theme_file_uri("/public/banner.webp");
                 $thumbnail_id = get_term_meta($cat_id, 'thumbnail_id', true);
                 $image_url = wp_get_attachment_url($thumbnail_id);
                 $catlabel =    get_field('catlabel', $cat_id);
+                echo $catlabel;
             ?>
                 <img src="<?php echo $image_url ?>" data-img data-image-id="<?php echo $category->slug; ?>" alt="">
 
